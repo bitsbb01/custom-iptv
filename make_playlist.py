371e6db3366801e4a48ecac50064bf527d3df637
keystroke3 <keystroke33@gmail.com>
2021-06-01 15:22:43 +0300

    Kenyan list now autogenerates

--- make_playlist.py
+++ make_playlist.py
@@ -1,8 +1,26 @@
 import requests
+import json
+
 
 def get_data(url):
     try:
         return requests.get(url).text.split("\n")
+    except Exception as e:
+        print(e)
+        exit(1)
+
+
+def getDailyMotion(channel, code):
+    try:
+        print(f"getting {channel}")
+        # res = requests.get(f'https://salvahe.co/headless?json=true&url=https://dai.ly/{code}')
+        res = requests.get(
+            f"http://127.0.0.1:8000/headless?json=true&url=https://dai.ly/{code}"
+        )
+        if res.status_code != 200:
+            print(f"could not get {channel}")
+            return "offline"
+        return res.json()["m3ulink"]
     except Exception as e:
         print(e)
         exit(1)
@@ -44,19 +62,35 @@ def make_esp():
         _spa.write("\n".join(spanish_channels))
 
 
+def make_ke():
+    with open("kenya.json") as f:
+        channels = json.load(f)
+
+    with open("m3u8/kenya.m3u8", "w") as m3u:
+        for channel in channels:
+            c_info = channels[channel]
+            if not c_info["link"]:
+                c_info["link"] = getDailyMotion(channel, c_info["id"])
+            extinf = f'#EXTINF:-1 tvg-name="{channel}" tvg-logo="{c_info["logo"]}" group-title="Kenya",{channel}\n'
+            link = f'{c_info["link"]}\n'
+            m3u.write(extinf)
+            m3u.write(link)
+
+
 def mergefiles(files, outfile):
     with open(outfile, "w") as out:
         for fname in files:
             with open(f"m3u8/{fname}.m3u8") as infile:
                 for line in infile:
                     out.write(line)
-            out.write('\n')
+            out.write("\n")
 
 
 if __name__ == "__main__":
-    filenames = ["kenya", "fashiontv", "english", "spanish"]
     make_eng()
     make_esp()
+    make_ke()
+    filenames = ["kenya", "fashiontv", "english", "spanish"]
     mergefiles(filenames, "m3u8/mylist.m3u8")
     filenames.remove("spanish")
     mergefiles(filenames, "m3u8/tv.m3u8")
