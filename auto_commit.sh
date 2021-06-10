50d5f5543fda5ea2871b58e11118987bf5eef7b6
keystroke3 <keystroke33@gmail.com>
2021-06-10 10:14:23 +0000

    fixed auto updater script

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 commit_time=$(date '+%Y-%m-%d-%H')
 cd /home/ted/custom-iptv/
-/usr/bin/python make_playlist.py >> make.log
+/usr/bin/python make_playlist_serverside.py >> make.log
 git add -A
 git commit -m "automatic update ${commit_time}"
 git push
