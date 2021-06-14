4be9d0187c9a6ce58a799a537044767d2d6b9019
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2021-06-14 23:59:22 +0300

    turned commit signing off for the bot

--- auto_commit.sh
+++ auto_commit.sh
@@ -3,5 +3,5 @@ commit_time=$(date '+%Y-%m-%d-%H')
 cd /home/ted/custom-iptv/
 /usr/bin/python make_playlist_serverside.py >> make.log
 git add -A
-git commit -m "[BOT] automatic update ${commit_time}"
+git -c commit.gpgsign=false commit -m  "[BOT] automatic update ${commit_time}"
 git push
