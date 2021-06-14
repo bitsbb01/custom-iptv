048dc26f1dfe2ccebe8d23042222b769a736863f
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2021-06-14 18:54:09 +0000

    changed commit message to show bot actions

--- auto_commit.sh
+++ auto_commit.sh
@@ -3,5 +3,5 @@ commit_time=$(date '+%Y-%m-%d-%H')
 cd /home/ted/custom-iptv/
 /usr/bin/python make_playlist_serverside.py >> make.log
 git add -A
-git commit -m "automatic update ${commit_time}"
+git commit -m "[BOT] automatic update ${commit_time}"
 git push
