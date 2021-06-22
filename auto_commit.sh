6516b023606b73e6970acbc29054c881d837bd8d
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2021-06-22 10:01:53 +0000

    removed update script

--- auto_commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-commit_time=$(date '+%Y-%m-%d-%H')
-cd /home/ted/custom-iptv/
-/usr/bin/python make_playlist_serverside.py >> make.log
-git add -A
-git commit --no-gpg-sign -m  "[BOT] automatic update ${commit_time}"
-git push
